81613327815b5f28c8d40e7b5e7a6e6432cb843e
dogaatac <dogaatac1905@gmail.com>
2023-12-14 06:50:55 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-echo -n "Memory gir dayi: "
-read num
+echo -n "Lütfen bellek boyutunu girin (varsayılan 16): "
+read -t 15 num  # Kullanıcıdan giriş bekler, 10 saniye içinde bir giriş yapılmazsa varsayılan değeri kullanır
+
+if [ -z "$num" ]; then
+    num=24  # Varsayılan bellek boyutu
+fi
 
 while true
 do
-  cd "$(dirname "$(realpath "${BASH_SOURCE[0]:-$0}")")"
+    cd "$(dirname "$(realpath "${BASH_SOURCE[0]:-$0}")")"
 
-  if [ -n "$num" ]; then
     mem_value="${num}G"
-  else
-    mem_value="24G"  # Memory gir dayi
-  fi
 
-  ./client -a xch16q8gxn8m8zs40fpfd6mja0hrdqwu9nna7newnww77sqjmudsa2tq89vzkk -d /mnt/pw/ --no-benchmark -c 14 --no-stop -vv -s /root/cache/ --no-mining --rescan-interval 60 -m "$mem_value"
+    ./client -a xch16q8gxn8m8zs40fpfd6mja0hrdqwu9nna7newnww77sqjmudsa2tq89vzkk -d /mnt/pw/ --no-benchmark -c 14 --no-stop -vv -s /root/cache/ --no-mining --rescan-interval 60 -m "$mem_value"
 
-  sleep 60
+    sleep 60
 done
