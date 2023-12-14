3b424d58c6fbd325897e67ffedf112a2696df3b3
dogaatac <dogaatac1905@gmail.com>
2023-12-14 06:05:24 +0300

    Update start.sh

--- linux/start.sh
+++ linux/start.sh
@@ -5,7 +5,7 @@ do
 
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]:-$0}")")"
 
-./client -a xch16q8gxn8m8zs40fpfd6mja0hrdqwu9nna7newnww77sqjmudsa2tq89vzkk -d /mnt/pw/ --no-benchmark -c 14 --no-stop -vv -s /root/cache/ --no-mining  --rescan-interval 60 
+./client -a xch16q8gxn8m8zs40fpfd6mja0hrdqwu9nna7newnww77sqjmudsa2tq89vzkk -d /mnt/pw/ --no-benchmark -c 14 --no-stop -vv -s /root/cache/ --no-mining  --rescan-interval 60 -m $num G
 
 sleep 60
 done
