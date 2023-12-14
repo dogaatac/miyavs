d640d54c042bc923f79b2e60ab3908fe7812348d
dogaatac <dogaatac1905@gmail.com>
2023-12-14 23:43:32 +0300

    Create up3.sh

---
+++ up3.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+try=0
+userRateLimitExceededCount=0
+slot=$((($RANDOM % 333) + 1))
+file_count=$(ls /root/.config/rclone/accounts | wc -l)
+
+while true
+do
+    index=$((($RANDOM % $file_count) + 1))
+               
+        rclone move /mnt/up3/ "ac$slot": --progress --drive-upload-cutoff 700G --multi-thread-streams 32 --tpslimit 3 --drive-stop-on-upload-limit --drive-chunk-size 128M --no-traverse --ignore-existing --log-level INFO   --drive-service-account-file "/root/.config/rclone/accounts/$index.json" -P
+        if [[ $? -eq 0 ]]; then
+            echo -e "\e[3;32m Transfer Done ... \e[0m"
+        else
+            echo -e "\e[3;31m Transfer Failed ... \e[0m"
+            if [[ $index -eq 1 ]]; then
+                userRateLimitExceededCount=$(($userRateLimitExceededCount + 1))
+            
+        fi
+
+        slot=$((slot % 333 + 1))
+        echo "********************************************************"
+    fi
+        sleep 5
+done
