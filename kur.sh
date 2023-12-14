fb7e10cd91601d4f2659408a89c58c8dbcd9d416
dogaatac <dogaatac1905@gmail.com>
2023-12-14 07:14:44 +0300

    Update kur.sh

--- kur.sh
+++ kur.sh
@@ -1,14 +1,27 @@
 chmod 777 *
+
 apt-get update -y
+
 apt install unzip nano screen ifstat -y
+
 sudo -v ; curl https://rclone.org/install.sh | sudo bash
+
 mkdir /mnt/temp pw up1 up2 up3 up4 up5 up6
+
 unzip config.zip
+
 screen -dmS mover bash mover.sh
+
 screen -dmS up1 bash up1.sh
+
 screen -dmS up2 bash up2.sh
+
 screen -dmS up3 bash up3.sh
+
 screen -dmS up4 bash up4.sh
+
 screen -dmS up5 bash up5.sh
+
 screen -dmS up6 bash up6.sh
+
 screen -S nossd bash start.sh
