c99fb395346a3147de777b7ea56e4a5e53a1d499
dogaatac <dogaatac1905@gmail.com>
2023-12-14 05:50:57 +0300

    Update startclientexample.sh

--- linux/startclientexample.sh
+++ linux/startclientexample.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+while true
+do
+
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]:-$0}")")"
 
 ./client -a xch16q8gxn8m8zs40fpfd6mja0hrdqwu9nna7newnww77sqjmudsa2tq89vzkk -d /mnt/pw/ --no-benchmark -c 14 --no-stop -vv -s /root/cache/ --no-mining  --rescan-interval 60 
+
+sleep 60
+done
