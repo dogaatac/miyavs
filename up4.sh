f165720854b9d661e7c5c8d4dfe74010b3838e04
dogaatac <dogaatac1905@gmail.com>
2023-12-20 06:52:40 +0300

    1

--- up4.sh
+++ up4.sh
@@ -4,38 +4,38 @@ import subprocess
 import time
 
 while True:
-    # Rclone config dosyasındaki "ac" ile başlayan satırları al
-    with open('/root/.config/rclone/yolla.conf', 'r') as file:
-        ac_lines = [line.strip() for line in file if line.startswith('[ac')]
+    try:
+        # Rclone config dosyasındaki "ac" ile başlayan satırları al
+        with open('/root/.config/rclone/yolla.conf', 'r') as file:
+            ac_lines = [line.strip() for line in file if line.startswith('[ac')]
 
-    # Rasgele bir "ac" satırını seç
-    selected_ac_line = random.choice(ac_lines)
+        # Rasgele bir "ac" satırını seç
+        selected_ac_line = random.choice(ac_lines)
 
-    # Seçilen "ac" satırından acX kısmını al
-    ac_name = selected_ac_line.split('[')[1].split(']')[0]
+        # Seçilen "ac" satırından acX kısmını al
+        ac_name = selected_ac_line.split('[')[1].split(']')[0]
 
-    # /root/.config/rclone/accounts/ klasöründe .json dosyalarını al
-    json_files = [file for file in os.listdir('/root/.config/rclone/accounts/') if file.endswith('.json')]
+        # /root/.config/rclone/accounts/ klasöründe .json dosyalarını al
+        json_files = [file for file in os.listdir('/root/.config/rclone/accounts/') if file.endswith('.json')]
 
-    # Rasgele bir .json dosyasını seç
-    selected_json_file = random.choice(json_files)
+        # Rasgele bir .json dosyasını seç
+        selected_json_file = random.choice(json_files)
 
-    # Bilgilendirme mesajı
-    print(f"Transfer başlatılıyor: {ac_name} kullanılarak {selected_json_file} service account'uyla transfer ediliyor...")
+        # Bilgilendirme mesajı
+        print(f"Transfer başlatılıyor: {ac_name} kullanılarak {selected_json_file} service account'uyla transfer ediliyor...")
 
-    # Rclone move komutunu oluştur
-    command = f'rclone move /mnt/up4/ "{ac_name}": --progress --config /root/.config/rclone/yolla.conf  --drive-upload-cutoff 700G --multi-thread-streams 32 --tpslimit 3 --drive-stop-on-upload-limit --drive-chunk-size 256M --no-traverse --ignore-existing --log-level INFO   --drive-service-account-file "/root/.config/rclone/accounts/{selected_json_file}" -P'
+        # Rclone move komutunu oluştur
+        command = f'rclone move /mnt/up4/ "{ac_name}": --progress --config /root/.config/rclone/yolla.conf --drive-upload-cutoff 700G --multi-thread-streams 32 --tpslimit 3 --drive-stop-on-upload-limit --drive-chunk-size 256M --no-traverse --ignore-existing --log-level INFO --drive-service-account-file "/root/.config/rclone/accounts/{selected_json_file}" -P'
 
-    # Komutu çalıştır
-    subprocess.run(command, shell=True)
+        # Komutu çalıştır
+        subprocess.run(command, shell=True)
 
-    # 1 dakika bekle
-    print("1 dakika bekleniyor...")
-    time.sleep(10)  # 1 dakika bekleme
+        # 1 dakika bekle
+        print("1 dakika bekleniyor...")
+        time.sleep(10)
 
-  except Exception as e:
+    except Exception as e:
         # Hata durumunda bilgilendirme
         print(f"Hata oluştu: {e}")
         print("Bekleme başlatılıyor...")
-        time.sleep(10)  # Hata durumunda da 1 dakika bekleyerek tekrar denemeye devam eder
-
+        time.sleep(10)
