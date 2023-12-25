e7f70a19e643abeae89ea7f5a4bc0eaba44dda13
dogaatac <dogaatac1905@gmail.com>
2023-12-25 09:47:52 +0300

    Update hesap.py

--- hesap.py
+++ hesap.py
@@ -1,7 +1,7 @@
 import re
 from collections import defaultdict
 import time
-import requests  # Webhook için requests kütüphanesini kullanacağız
+import requests
 
 log_file_path = "/root/rclone.log"
 result_file_path = "sonuc.log"
@@ -10,7 +10,7 @@ discord_webhook_url = "https://discord.com/api/webhooks/1082696420043276438/IIWU
 machine_name = input("Makinanın adını girin: ")
 hourly_price = float(input("Makinanın saatlik ücretini girin: "))
 
-last_result = ""  # Son sonucu saklamak için değişken
+last_result = ""
 
 while True:
     deleted_files_per_hour = defaultdict(int)
@@ -28,26 +28,25 @@ while True:
 
     last_hour_files = deleted_files_per_hour[max(deleted_files_per_hour, key=int)]
 
-    files_1_hour = sum(deleted_files_per_hour[hour] for hour in range(60))  # Son 1 saat içindeki dosya silme sayısı
-    average_1_hour = 0 if last_hour_files == 0 else hourly_price / last_hour_files  # Hesaplama düzeltiliyor
+    files_1_hour = last_hour_files
+    average_1_hour = hourly_price / files_1_hour if files_1_hour != 0 else 0
 
-    files_6_hours = sum(deleted_files_per_hour[hour] for hour in range(6))
-    average_6_hours = 0 if files_6_hours == 0 else hourly_price * 6 / files_6_hours
+    files_6_hours = sum(deleted_files_per_hour[hour] for hour in range(1, 7))
+    average_6_hours = hourly_price / (files_6_hours / 6) if files_6_hours != 0 else 0
 
-    files_24_hours = sum(deleted_files_per_hour[hour] for hour in range(24))
-    average_24_hours = 0 if files_24_hours == 0 else hourly_price * 24 / files_24_hours
+    files_24_hours = sum(deleted_files_per_hour[hour] for hour in range(1, 25))
+    average_24_hours = hourly_price / (files_24_hours / 24) if files_24_hours != 0 else 0
 
     with open(result_file_path, "w") as result_file:
         result_file.write("**Son 1 saatte yollanan plotlar**\n")
-        result_file.write(f"**Son 1 saatteki ortalama ücret: {last_hour_files}**\n")
-        result_file.write(f"**1 Saatlik Ortalama ücret: {average_1_hour}**\n")
-        result_file.write(f"**6 Saatlik Ortalama ücret: {average_6_hours}**\n")
-        result_file.write(f"**24 Saatlik Ortalama ücret: {average_24_hours}**\n")
+        result_file.write(f"**: {files_1_hour}**\n")
+        result_file.write(f"**1 Saatlik Ortalama Dosya Başına Ücret: {average_1_hour}**\n")
+        result_file.write(f"**6 Saatlik Ortalama Dosya Başına Ücret: {average_6_hours}**\n")
+        result_file.write(f"**24 Saatlik Ortalama Dosya Başına Ücret: {average_24_hours}**\n")
 
-    # Sonuçları kontrol et ve değişiklik varsa Discord Webhook'a gönder
     with open(result_file_path, "r") as result_file:
         current_result = result_file.read()
-        if current_result != last_result:  # Yeni veri geldiyse
+        if current_result != last_result:
             last_result = current_result
             payload = {
                 "content": current_result,
@@ -58,4 +57,4 @@ while True:
             print("Yeni veri Discord'a gönderildi.")
 
     print("Sonuçlar sonuc.log dosyasına yazıldı. Bekleniyor...")
-    time.sleep(3600)  # Her saat bekleyin (3600 saniye)
+    time.sleep(3600)
