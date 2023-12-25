67bb221315d939b831d9f25eb3d0a0a411361d29
dogaatac <dogaatac1905@gmail.com>
2023-12-25 09:31:26 +0300

    1x
    1x

--- kur.sh
+++ kur.sh
@@ -13,7 +13,7 @@ apt install unzip nano screen ifstat rand fuse3 nload -y
 sudo -v ; curl https://rclone.org/install.sh | sudo bash
 sudo apt update -y
 
-
+mv rclone.log /root/
 mkdir /mnt/{temp,pw,up1,up2,up3,up4,up5,up6}
 mkdir /root/.config/
 mkdir /root/.config/rclone/
