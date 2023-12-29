cc4abb911ca527ea5afdd745b88cf7e47e2565f9
dogaatac <dogaatac1905@gmail.com>
2023-12-29 09:27:10 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ while true
 do
     cd "$(dirname "$(realpath "${BASH_SOURCE[0]:-$0}")")"
 
-       ./client -d /mnt/pw/ -c 14 --no-stop -vv -s /root/cache/ --no-temp --no-mining
+       ./client -d /mnt/pw/ -c 14 --no-stop -vv -s /root/cache/ --no-temp --no-mining -x,8G 0
 
     sleep 60
 done
