d9a1bf2414569482f0f2c88c6a6477d58c885110
dogaatac <dogaatac1905@gmail.com>
2024-07-04 17:16:41 +0300

    1x

--- move.sh
+++ move.sh
@@ -1,8 +1,8 @@
 while true
 do
-    random_sleep=$((1 + RANDOM % 7))  # 1 ile 30 arasında rastgele bir sayı oluşturur
+    random_sleep=$((1 + RANDOM % 5))  # 1 ile 30 arasında rastgele bir sayı oluşturur
 
-    mv /mnt/pw/*.fpt /mnt/up$((1 + RANDOM % 7))  # Dosyaları rastgele bir "up" klasörüne taşır
+    mv /mnt/pw/*.fpt /mnt/up$((1 + RANDOM % 5))  # Dosyaları rastgele bir "up" klasörüne taşır
 
     sleep $random_sleep  # Rasgele oluşturulan süre kadar bekleme yapar
 done
