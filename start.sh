44b5280bc186c20ec03ca3d471255dedb3fc77d5
dogaatac <dogaatac1905@gmail.com>
2025-01-15 20:57:44 +0300

    2x

--- start.sh
+++ start.sh
@@ -7,10 +7,10 @@ while true; do
        cd "$(dirname "$(realpath "${BASH_SOURCE[0]:-$0}")")"
 
         if [ "$TOTAL_RAM" -ge 110 ]; then
-                ./client -d /mnt/pw/  -c 10 --no-stop -vv -s /root/cache/ --no-temp --no-mining --no-benchmark 
+                ./anan -d /mnt/pw/  -c 10 --no-stop -vv -s /root/cache/ --no-temp --no-mining --no-benchmark 
     else
         
-        ./client -d,1N /mnt/pw/ -c 10  -vv -s /root/cache/ --no-mining --no-benchmark --rescan-interval 100 --no-stop
+        ./anan -d,1N /mnt/pw/ -c 10  -vv -s /root/cache/ --no-mining --no-benchmark --rescan-interval 100 --no-stop
     fi
 
     # 60 saniye bekle
