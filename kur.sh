31194845f2c07e98b44efcfba71402efd08f60a8
Eray <37026396+eraemm@users.noreply.github.com>
2025-03-30 22:31:32 +0300

    1x

--- kur.sh
+++ kur.sh
@@ -12,6 +12,7 @@ apt install unzip nano screen ifstat rand fuse3 nload -y
 sudo apt-get install inotify-tools -y
 sudo -v ; curl https://rclone.org/install.sh | sudo bash
 mv rclone.log /root/
+mkdir /root/pw
 mkdir /mnt/{pw,up1,up2,up3}
 mkdir /root/.config/
 mkdir /root/.config/rclone/
--- start.sh
+++ start.sh
@@ -7,10 +7,10 @@ while true; do
        cd "$(dirname "$(realpath "${BASH_SOURCE[0]:-$0}")")"
 
         if [ "$TOTAL_RAM" -ge 110 ]; then
-                ./client -d /mnt/pw/  -c 10 --no-stop -vv -s /root/cache/ --no-temp --no-mining --no-benchmark 
+                ./client -d /root/pw/  -c 10 --no-stop -vv -s /root/cache/ --no-temp --no-mining --no-benchmark 
     else
         
-        ./client -d,1N /mnt/pw/ -c 10  -vv -s /root/cache/ --no-mining --no-benchmark  --no-stop
+        ./client -d,1N /root/pw/ -c 10  -vv -s /root/cache/ --no-mining --no-benchmark  --no-stop
     fi
 
     # 60 saniye bekle
