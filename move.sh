a92b12d01289910fcff1341a7b8723c5294d6db9
dogaatac <dogaatac1905@gmail.com>
2025-03-30 23:56:30 +0300

    Update move.sh

--- move.sh
+++ move.sh
@@ -13,15 +13,28 @@ while true; do
         echo "Gönderilecek .fpt dosyası bulunamadı."
     else
         for file in "${files[@]}"; do
+            # Dosyanın temel adını al (uzantısız)
+            base_name=$(basename "$file" .fpt)
+            
             # Rastgele bir hedef dizin seç
             RANDOM_DEST=${DEST_DIRS[$RANDOM % ${#DEST_DIRS[@]}]}
             
-            # Dosyayı hedef dizine taşı
-            mv "$file" "$RANDOM_DEST"/
-            echo "$file dosyası $RANDOM_DEST dizinine taşındı."
+            # Geçici dosya adı (örneğin .tmp uzantısıyla)
+            temp_name="${base_name}.tmp"
+            
+            # Dosyayı geçici adla hedef dizine taşı
+            mv "$file" "$RANDOM_DEST/$temp_name"
+            
+            # Eğer taşıma başarılıysa, .fpt uzantısını geri döndür
+            if [ $? -eq 0 ]; then
+                mv "$RANDOM_DEST/$temp_name" "$RANDOM_DEST/${base_name}.fpt"
+                echo "$file dosyası $RANDOM_DEST/${base_name}.fpt olarak taşındı."
+            else
+                echo "Hata: $file taşınamadı."
+            fi
         done
     fi
     
-    # 60 saniye bekle
-    sleep 252
+    # 252 saniye bekle
+    sleep 222
 done
